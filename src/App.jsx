617d920f47cd49a10fb52897a2617b42f4773dd7
Sai Kishore <sai2009kishore@gmail.com>
2025-04-26 16:52:27 -0500

    Route testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,23 @@ import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Projects from './components/Projects';
 import NavbarComponent from './components/NavbarComponent';
-import { getContext } from './assets/js/utils';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/App.css';
 import './assets/styles/scrollbar.css';
 
 function App() {
-  const context = getContext();
-
   return (
     <div id="app">
       <NavbarComponent />
 
       <Routes>
-        <Route path={`${context}home`} element={<Home />} />
-        <Route path={`${context}about`} element={<About />} />
-        <Route path={`${context}resume`} element={<Resume />} />
-        <Route path={`${context}projects`} element={<Projects />} />
-        <Route path={`${context}interests`} element={<Interests />} />
-        <Route path={`${context}contact`} element={<Contact />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/interests' element={<Interests />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
 
       <Footer />
