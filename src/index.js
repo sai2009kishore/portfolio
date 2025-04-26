69f19c24bdb1a1ef8615e258c1f5f4b9e14a4333
Sai Kishore <sai2009kishore@gmail.com>
2025-04-26 16:37:36 -0500

    Fixed bug with routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 import './assets/styles/index.css';
+import { getContext } from './assets/js/utils';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/">
+    <HashRouter basename={getContext()}>
       <App />
     </HashRouter>
   </React.StrictMode>
